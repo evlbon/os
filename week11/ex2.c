3296ad762d77e2cac0a2a64cefadad020e2bd73b
laser4622 <36008917+laser4622@users.noreply.github.com>
2018-11-01 22:57:57 +0300

    Add files via upload

---
+++ week11/ex2.c
@@ -0,0 +1,14 @@
+//
+// Created by Eugene on 01/11/2018.
+//
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    printf("H");sleep(1);
+    printf("e");sleep(1);
+    printf("l");sleep(1);
+    printf("l");sleep(1);
+    printf("o\n");
+    return 0;
+}
